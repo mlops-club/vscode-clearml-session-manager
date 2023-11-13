7e1c8ba1d0ca2c15d9b818f7faa89ff8a8c5f5c2
Eric Riddoch <eric.riddoch@benlabs.com>
2023-11-13 00:12:14 -0500

    patch: preventing tasks from being logged to file

--- src/common/clearml/api-client.ts
+++ src/common/clearml/api-client.ts
@@ -109,7 +109,7 @@ export class ClearMLApiClient {
 
         // write tasks to disk as a json file, use 2-space indenting
         const tasksJson = JSON.stringify(tasks, null, 2);
-        const tasksJsonFpath = "/Users/ericriddoch/repos/extra/hello-world-vscode-ext/clearml-session-manager/tasks.json";
+        // const tasksJsonFpath = "/Users/ericriddoch/repos/extra/hello-world-vscode-ext/clearml-session-manager/tasks.json";
         const fs = require('fs').promises;
         await fs.writeFile(tasksJsonFpath, tasksJson);
 
